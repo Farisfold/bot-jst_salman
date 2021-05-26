61b763004624506347432d69d120088a231148b4
Farisfold <84666834+Farisfold@users.noreply.github.com>
2021-05-26 13:06:16 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,40 +28,15 @@ bot.onText(/\/menu/, (msg) => {
     );   
 });
 
-State = 0;
+
 bot.onText(/\/predict/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `masukan nilai i|v contohnya 9|9`
     );
-    State = 1;
 });
 
-bot.on ('messsage', (msg) => { 
-    If (State == 1){
-        console.log(msg.text);
-        s = msg.text.split ("|");
-        i = s[0]
-        v = s[1]
-        model.predict(
-        [
-            parseFloat(s[0]), // string to float
-            parseFloat(s[1])
-        ]
-    ).then((jres)=>{
-       bot.sendMessage(
-        msg.chat.id,
-        `nilai v yang diprediksi adalah s{jres=0}volt`
-     );
-        bot.sendMessage(
-        msg.chat.id,
-        `nilai p yang diprediksi adalah s{jres=1}watt`
-     );
-    })
-}else{
-      State = 0
-           }
-})
+
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
